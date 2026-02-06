fd0e238d9b3f00f04de4d2cd2cfc0e57e81f40af
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-02-06 08:36:22 -0300

    feat: Exeption Handler and links

--- dslist-backend/src/main/java/com/joaodev/dslist/controllers/assembler/GameListModelAssembler.java
+++ dslist-backend/src/main/java/com/joaodev/dslist/controllers/assembler/GameListModelAssembler.java
@@ -1,16 +1,26 @@
 package com.joaodev.dslist.controllers.assembler;
 
-import org.springframework.hateoas.server.RepresentationModelAssembler;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import com.joaodev.dslist.controllers.GameListController;
 import com.joaodev.dslist.dto.GameListDTO;
 import com.joaodev.dslist.entities.GameList;
 
+@Component
 public class GameListModelAssembler implements RepresentationModelAssembler<GameList, GameListDTO> {
 
     @Override
     public GameListDTO toModel(GameList entity) {
-        
+
         GameListDTO dto = new GameListDTO(entity);
+
+        dto.add(linkTo(methodOn(GameListController.class).findGames(entity.getId())).withSelfRel());
+
+        dto.add(linkTo(methodOn(GameListController.class).findAll(null, null)).withRel("all-lists"));
 
         return dto;
     }
--- dslist-backend/src/main/java/com/joaodev/dslist/controllers/assembler/GameMinModelAssembler.java
+++ dslist-backend/src/main/java/com/joaodev/dslist/controllers/assembler/GameMinModelAssembler.java
@@ -1,8 +1,12 @@
 package com.joaodev.dslist.controllers.assembler;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
+import com.joaodev.dslist.controllers.GameController;
 import com.joaodev.dslist.dto.GameMinDTO;
 import com.joaodev.dslist.entities.Game;
 
@@ -13,6 +17,10 @@ public class GameMinModelAssembler implements RepresentationModelAssembler<Game,
     public GameMinDTO toModel(Game entity) {
         
         GameMinDTO dto = new GameMinDTO(entity);
+
+        dto.add(linkTo(methodOn(GameController.class).findById(entity.getId())).withSelfRel());
+
+        dto.add(linkTo(methodOn(GameController.class).findAll(null, null)).withRel("all-games"));
 
         return dto;
     }
